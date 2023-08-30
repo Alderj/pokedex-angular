2f52b2d4d7372115ac9b9cf1ebd44a7cde7fa4af
Alder Junio <33815682+Alderj@users.noreply.github.com>
2023-08-30 10:26:49 -0400

    commit atualizações

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -23,7 +23,7 @@ export class CardComponent implements OnInit{
   ){ }
 
     ngOnInit(): void {
-      this.getPokemon('squirtle')
+      this.getPokemon('pikachu')
     }
 
     getPokemon (searchName: string){
