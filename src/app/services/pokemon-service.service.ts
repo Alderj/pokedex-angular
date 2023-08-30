366a740d6c30efc4a79cb45ec23dc28b3deba60a
Alderj <33815682+Alderj@users.noreply.github.com>
2023-08-29 22:28:06 -0400

    criado pokemonService

--- src/app/services/pokemon-service.service.ts
+++ src/app/services/pokemon-service.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class PokemonServicesService {
+export class PokemonServiceService {
 
   constructor() { }
 }
